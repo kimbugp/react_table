6e363ebbe9c57b9b24f18f2b0fd9f76d5d5b4479
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2020-02-19 12:36:17 +0300

    remove sample render

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import SimpleTable from "./App";
-
-ReactDOM.render(<SimpleTable />, document.getElementById("root"));
-
-// module.exports = require("./App");
+module.exports = require("./App");
